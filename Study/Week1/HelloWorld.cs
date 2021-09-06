dad2e8a7b20efe2e7eda4bca23cef6f662d6d72c
littlebuffett <76998855+littlebuffett@users.noreply.github.com>
2021-09-06 13:41:02 +0900

    Create HelloWorld.cs

---
+++ Study/Week1/HelloWorld.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch1
+{
+    class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            //한줄 주석은 이렇게
+            Console.WriteLine("Hello World...");
+            /*
+                1
+                2
+                3
+                4
+            */
+            }
+        }
+    }
