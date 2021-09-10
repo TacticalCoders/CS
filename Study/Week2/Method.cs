5db233573c0a04614b16ebc10f83c451495262e3
littlebuffett <76998855+littlebuffett@users.noreply.github.com>
2021-09-10 21:52:17 +0900

    Add files via upload

---
+++ Study/Week2/Method.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch2_Method
+{
+    class Method
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            //int tot = a + b;
+            //Console.WriteLine("a + b = {0}", tot);
+            int hap = Plus.Sum(a, b);
+            Console.WriteLine("a + b = {0}", hap);
+        }
+
+        //public class Plus {
+        //    public static int Sum(int c, int d)
+        //    {
+        //        int tot = c + d;
+        //        return tot;
+        //    }
+        //}
+        //같은 클래스 내에서는 public, private 상관 없음.
+        
+    }
+}
---
+++ Study/Week2/Plus.cs
@@ -0,0 +1,8 @@
+﻿public class Plus
+{
+    public static int Sum(int c, int d)
+    {
+        int tot = c + d;
+        return tot;
+    }
+}
