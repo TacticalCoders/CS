e97a121753d48635e7125fbecd208bd1acba0f75
병하 강 <76998855+littlebuffett@users.noreply.github.com>
2021-10-06 14:52:18 +0900

    Update 성적처리 1단계.cs

--- Study/Week4/성적처리 1단계.cs
+++ Study/Week4/성적처리 1단계.cs
@@ -6,31 +6,44 @@ using System.Threading.Tasks;
 
 namespace ch4_배열
 {
-    class 성적처리_1단계
+    class 성적처리_2단계
     {
         static void Main(string[] args)
         {
-            int[,] jumsuData = new int[4, 7] { { 1, 33,44,55, 0, 0, 0 },
+            int[,] jumsuData = new int[4, 7] { { 1, 33, 44, 55, 0, 0, 0 },
                                                { 2, 77, 88, 99, 0, 0, 0 },
                                                { 3, 55, 66, 77, 0, 0, 0 },
                                                { 4, 87, 88, 89, 0, 0, 0 } };
 
-            Console.WriteLine("학번\t국어\t영어\t수학\t총점\t평균\t석차");
-            Console.WriteLine("=====================================================");
+            string[] name = new string[] { "KBS", "SBS", "YTN", "MBC" };
+
+            int sum = 0;
+
+            for(int i = 0; i <jumsuData.GetLength(0); i++)
+            {
+                for(int j = 1; j <jumsuData.GetLength(1)-3; j++)
+                {
+                    sum += jumsuData[i, j];
+                }
+                jumsuData[i, 4] = sum;
+                jumsuData[i, 5] = sum / 3;
+                sum = 0;
+            }
+
+            Console.WriteLine("학번\t이름\t국어\t영어\t수학\t총점\t평균\t석차");
+            Console.WriteLine("============================================================");
             for (int i = 0; i < jumsuData.GetLength(0); i++)
             {
-                for(int j = 0; j < jumsuData.GetLength(1); j++)
+                for (int j = 0; j < jumsuData.GetLength(1); j++)
                 {
-                    if (j>0 && j < 4)
-                        jumsuData[i, 4] += jumsuData[i, j];
-                    else if (j == 4)
-                        jumsuData[i, 5] = jumsuData[i, 4] / 3;
+                    
+                    if (j == 1) Console.Write(name[i]+"\t");
 
-                    Console.Write("{0}\t",jumsuData[i,j]);
+                    Console.Write("{0}\t", jumsuData[i, j]);
                 }
                 Console.WriteLine();
             }
-            Console.WriteLine("-----------------------------------------------------");
+            Console.WriteLine("------------------------------------------------------------");
         }
     }
 }
