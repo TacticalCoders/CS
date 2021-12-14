fdb6aaca3d7edd6f50b804cd5e959ecc247f4d1b
강병하 <76998855+littlebuffett@users.noreply.github.com>
2021-12-14 22:10:19 +0900

    Add files via upload

---
+++ Study/랜덤시험문제/랜덤넘버발생.Designer.cs
@@ -0,0 +1,154 @@
+﻿
+namespace 강병하_랜덤시험문제
+{
+    partial class 랜덤넘버발생
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "문제 중복 제거 전";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "문제 중복 제거 후";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 141);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "보기 중복 제거 전";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(27, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(101, 12);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "보기 중복 제거 후";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(165, 37);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 12);
+            this.label5.TabIndex = 4;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(161, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 12);
+            this.label6.TabIndex = 5;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(165, 141);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(0, 12);
+            this.label7.TabIndex = 6;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(161, 182);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(0, 12);
+            this.label8.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(99, 226);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Test";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // 랜덤넘버발생
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "랜덤넘버발생";
+            this.Text = "랜덤넘버발생";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Study/랜덤시험문제/랜덤넘버발생.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 강병하_랜덤시험문제
+{
+    public partial class 랜덤넘버발생 : Form
+    {
+        public int[] randQuestion = new int[3];
+        public int[] randBogi = new int[4];
+        string Qhistory = "";
+        string Bhistory = "";
+        int QuestionIndex = 0, BogiIndex = 0;
+        Random rand = new Random();
+
+        public 랜덤넘버발생()
+        {
+            InitializeComponent();
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label5.Text = makeRandQuestion();
+            label6.Text = getQuestionStr();
+            label7.Text = makeRandBogi();
+            label8.Text = getBogiStr();
+        }
+        private bool isSameQ(int i, int value )
+        {
+            for(int j = 0; j < i; j++)
+                if (randQuestion[j] == value) return true;
+            return false;
+        }
+        private bool isSameB(int i, int value )
+        {
+            for (int j = 0; j < i; j++)
+                if (randBogi[j] == value) return true;
+            return false;
+        }
+        public string makeRandQuestion()
+        {
+            Qhistory = "";
+            QuestionIndex = 0;
+            for (int i = 0; QuestionIndex != 3; i++)
+            {
+                randQuestion[i] = rand.Next(5); //일단 추가
+                Qhistory = Qhistory + Convert.ToString(randQuestion[i]) + " ";
+
+                //지금까지 추가된 원소 중 중복이 발생하면 
+                if (isSameQ(i, randQuestion[i])) i--;
+                else QuestionIndex++;
+            }
+
+            return Qhistory;
+        }
+        public string makeRandBogi()
+        {
+            Bhistory = "";
+            BogiIndex = 0;
+            for (int m = 0; BogiIndex != 4; m++)
+            {
+                randBogi[m] = rand.Next(1,5); //일단 추가
+                Bhistory = Bhistory + Convert.ToString(randBogi[m]) + " ";
+
+                //지금까지 추가된 원소 중 중복이 발생하면 
+                if (isSameB(m, randBogi[m])) m--;
+                else BogiIndex++;
+
+            }
+            return Bhistory;
+        }
+        public string getQuestionStr()
+        {
+            string questionResult = "";
+            for (int k = 0; k < randQuestion.Length; k++)
+            {
+                questionResult = questionResult + Convert.ToString(randQuestion[k]) + " ";
+            }
+            return questionResult;
+        }
+        public string getBogiStr()
+        {
+            string bogiResult = "";
+            for (int p = 0; p < randBogi.Length; p++)
+            {
+                bogiResult = bogiResult + Convert.ToString(randBogi[p]) + " ";
+            }
+            return bogiResult;
+        }
+    }
+}
